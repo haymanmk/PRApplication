238e1aaf90e10a8f1d0ac4b2b59be6243ace6f5e
haymanmk <haymanmk@gmail.com>
2023-05-11 18:20:24 +0800

    temp

--- src/hooks/use-get-cookies.js
+++ src/hooks/use-get-cookies.js
@@ -3,32 +3,55 @@ const { useEffect, useRef } = require("react");
 export function useGetCookies() {
   let setTimeout_ID = useRef();
   useEffect(() => {
-    const extensionID = "glcaenamkkmjnpmaedecbgimgdeaocck";
+    const extensionID = "polabndodhphfbdfdabhbdhbhfohekfh";
     const message = {
-      openURL: "https://developer.chrome.com/docs/extensions/reference/runtime/#method-sendMessage",
-      getCookies: "leetcode.com",
+      // openURL: "https://developer.chrome.com/docs/extensions/reference/runtime/#method-sendMessage",
+      getCookies: "shiwpa-etrex9.garmin.com",
+      getCookies: "linxpa-bpm-cluster.garmin.com",
+      getCookies: ".garmin.com",
     };
+    let cookies = "";
 
     try {
+      setTimeout_ID.current = setTimeout(
+        () => console.log("GetCookies Extension no Response."),
+        5000
+      );
       Object.keys(message).map((key) => {
-        setTimeout_ID.current = setTimeout(
-          () => console.log("GetCookies Extension no Response."),
-          5000
-        );
-
         const msg = { [key]: message[key] };
         chrome.runtime.sendMessage(extensionID, msg, (res, err) => {
           if (err) console.error(err);
-          else if (res) {
+          else if (res.length) {
             console.log(res);
+            res.map((cookie) => {
+              cookies += `${cookie.name}=${cookie.value};`;
+            });
           }
-          clearTimeout(setTimeout_ID.current);
+          if (cookies) getMyPR(cookies);
         });
       });
+      clearTimeout(setTimeout_ID.current);
     } catch (err) {
       console.error(err);
       // console.log("GetCookies Extension not Found!");
+
+      clearTimeout(setTimeout_ID.current);
       return;
     }
   }, []);
+}
+
+function getMyPR(cookies) {
+  const _dc = Date.now();
+  let url = `https://shiwpa-etrex9.garmin.com:9099/FINSystem/PrQueryMyPrRecent.action?_dc=${_dc}`;
+  let options = {
+    method: "POST",
+    headers: {
+      Cookie: cookies,
+    },
+    mode: "no-cors",
+  };
+  fetch(url, options).then((response) => {
+    console.log(response);
+  });
 }
